1b53544094b53611b745e6950e8684fda75f19c8
JulianaPelozo <julianapelozopacheco@gmail.com>
2025-10-30 16:58:08 -0300

    save app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 import exemploRoutes from './routes/exemploRoutes'
-import loginRoutes from './routes/loginRouter'
+import loginRoutes from './routes/Router'
 
 dotenv.config()
 
