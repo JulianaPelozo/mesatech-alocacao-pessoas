7cae8f5c388c0c885fb0d0b3a1838a3f8f01f66d
Hfaaf <heitorfariaspinheirodoamaral@gmail.com>
2025-12-04 23:14:55 -0300

    Commit back

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,12 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://cozy-pony-c916c6.netlify.app/',
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
   credentials: true
 }));
 app.use(express.json());
